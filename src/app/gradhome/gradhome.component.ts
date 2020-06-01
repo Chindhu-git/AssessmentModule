df76e65a5bede2ec89b42b95065cc67e09d1e815
Chindhu-git <chindhu.alagappan@accoliteindia.com>
2020-06-01 20:38:49 +0530

    fifth commit

--- src/app/gradhome/gradhome.component.ts
+++ src/app/gradhome/gradhome.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {GradComponent} from '/home/chindhu/assessmentProj/src/app/grad/grad.component';
 
 @Component({
   selector: 'app-gradhome',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GradhomeComponent implements OnInit {
 
+  email: String;
   constructor() { }
 
   ngOnInit(): void {
