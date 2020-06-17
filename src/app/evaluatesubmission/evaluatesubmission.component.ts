74b5d5349f78dc50a974849e0ef60329c61c92ae
Chindhu-git <chindhu.alagappan@accoliteindia.com>
2020-06-17 18:50:10 +0530

    twentyfive commit

--- src/app/evaluatesubmission/evaluatesubmission.component.ts
+++ src/app/evaluatesubmission/evaluatesubmission.component.ts
@@ -32,7 +32,7 @@ export class EvaluatesubmissionComponent implements OnInit {
     this.uploadedfile=sessionStorage.getItem('UploadedFile');
     this.score=sessionStorage.getItem('UploadedScore');
 
-    var file = new Blob([this.uploadedfile], {type: 'text/plain'});
+    var file = new Blob([this.uploadedfile], {type: 'text'});
     const new_file=this.bin2string(file);
     //var file = new File([blob], this.filename, {type: 'application/pdf', lastModified: Date.now()});  
     saveAs(new_file,this.filename);
