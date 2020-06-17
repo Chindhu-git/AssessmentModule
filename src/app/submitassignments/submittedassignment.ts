eb085040585f7a4afbea8592c2eb563c810deeb5
Chindhu-git <chindhu.alagappan@accoliteindia.com>
2020-06-17 14:00:23 +0530

    twentythree commit

--- src/app/submitassignments/submittedassignment.ts
+++ src/app/submitassignments/submittedassignment.ts
@@ -2,12 +2,12 @@ export class SubmittedAssignment
 {
     assignment_name :string
     email: string;
+    filepath:string;
     filename: string;
-    uploadfile: any;
     constructor(
         assignment_name :string,
         email:string,
+        filepath:string,
         filename:string,
-        uploadfile: any
     ){}
 }
